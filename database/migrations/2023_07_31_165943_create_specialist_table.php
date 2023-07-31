37d934f8403d54240468401e88777be1ac5743df
Rezacrown <aryhidayatfebriana@gmail.com>
2023-08-01 02:02:41 +0800

    migrate model database

---
+++ database/migrations/2023_07_31_165943_create_specialist_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpecialistTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('specialist', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('pricing');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('specialist');
+    }
+}
---
+++ database/migrations/2023_07_31_172210_create_docter_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocterTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('docter', function (Blueprint $table) {
+            $table->id();
+            $table->integer('specialist_id');
+            $table->string('name');
+            $table->string('fee');
+            $table->text('photo')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('docter');
+    }
+}
