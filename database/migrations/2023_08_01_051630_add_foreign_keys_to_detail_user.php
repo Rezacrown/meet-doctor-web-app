91ecbecda14c3529554f8af7c6431e8395b53bf9
Rezacrown <aryhidayatfebriana@gmail.com>
2023-08-01 14:05:54 +0800

    add foreign key sample

---
+++ database/migrations/2023_08_01_051630_add_foreign_keys_to_detail_user.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeysToDetailUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('detail_user', function (Blueprint $table) {
+            $table->foreign('user_id', 'fk_detail_user_to_users')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('type_user_id', 'fk_detail_user_to_type_user')->references('id')->on('type_user')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('detail_user', function (Blueprint $table) {
+            //
+            $table->dropForeign('fk_detail_user_to_users');
+            $table->dropForeign('fk_detail_user_to_type_user');
+        });
+    }
+}
